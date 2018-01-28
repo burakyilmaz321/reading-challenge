359eac4847f4adee6f9671e058b9599b04c195c6
burakyilmaz321 <burakyilmaz321@gmail.com>
2018-01-28 18:52:24 +0300

    finish implementing create method for reading

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -8,4 +8,19 @@ class ReadingsController < ApplicationController
   def new
     @reading = current_user.readings.new
   end
+
+  def create
+    @reading = current_user.readings.new(reading_params)
+    if @reading.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def reading_params
+    params.require(:reading).permit(:status, :month, book_attributes: [:title, :page_num, author_attributes: [:name]])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'readings#index'
-  resources :readings, only: [:index, :new]
+  resources :readings, only: [:index, :new, :create]
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
