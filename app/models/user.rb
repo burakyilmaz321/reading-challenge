c72cc354152331f1184baea4e4984bfe87db1c8a
burakyilmaz321 <burakyilmaz321@gmail.com>
2018-01-31 20:49:04 +0300

    delete readings of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :readings
+  has_many :readings, dependent: :destroy
   has_many :books, through: :readings
 end
