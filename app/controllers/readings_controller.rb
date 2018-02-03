b4046cc88ed015519e0e8893ceb8d74609739059
burakyilmaz321 <burakyilmaz321@gmail.com>
2018-02-03 17:20:04 +0300

    letsencrypt not in reading controller anymore

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -1,5 +1,5 @@
 class ReadingsController < ApplicationController
-  before_action :authenticate_user!, :except => [:letsencrypt]
+  before_action :authenticate_user!
 
   def index
     if user_signed_in?
